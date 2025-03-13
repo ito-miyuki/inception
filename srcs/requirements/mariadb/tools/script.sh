a6ae6f647f13b936a99454422634bc3ea43a9c05
ito-miyuki <mitou.1221@gmail.com>
2025-03-13 15:57:30 +0200

    changed nginx

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,18 +5,24 @@ chown -R mysql:mysql /var/run/mysqld
 chown -R mysql:mysql /var/log/mysql
 chown -R mysql:mysql /var/lib/mysql
 
-#mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+# init mariadb data directory
 mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
 echo "Initializing MariaDB database..."
+
+# start mariadb temporary in bootstrap mode to set up initial users and databases
 mysqld --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
 
-#For security reasons
+# remove anonymous users for security
 DELETE FROM mysql.user WHERE User='';
+
+# remove test database as it's not needed
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
+
+# restrict root access to localhost for security
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
 #Set root password
@@ -29,7 +35,11 @@ CREATE DATABASE $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PWD';
 GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
 GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PWD' WITH GRANT OPTION;
+
+# allow the worspress user to read system tables
 GRANT SELECT ON mysql.* TO '$WP_DB_USER'@'%';
+
+# apply changes
 FLUSH PRIVILEGES;
 EOF
 
--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Setting correct permissions for /var/www/html..."
 
-chwon -R nginx:nginx /var/www/html
+chown -R nginx:nginx /var/www/html
 chmod -R 755 /var/www/html
 
 echo "Starting Nginx..."
